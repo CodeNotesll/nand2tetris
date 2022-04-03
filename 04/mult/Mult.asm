c60b80fdfcfaa28859a9598abd2159020814e3a6
wangshiwen <Wse1714401046@gmail.com>
2022-04-03 16:28:45 +0800

    complete Mult.asm and pass the test

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,37 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R1
+  D=M
+@multi
+  M=D  // multi = R1
+@product
+  M=0
+@R0
+  D=M
+@n
+  M=D   // n = R0
+(LOOP)
+  @n
+    D=M
+  @POSITIVE
+    D;JGT  // n > 0
+  // n==0
+  @product
+    D=M
+  @R2
+    M=D  // R2 = product = R1*R0
+  @END
+    0;JMP  // goto end
+  (POSITIVE) // product = product + multi; n = n - 1
+    @multi
+      D=M
+    @product
+      M=D+M  // product = product + multi
+    @n
+      M=M-1 // n = n -1
+    @LOOP
+      0;JMP // goto loop
+(END)
+  @END
+    0;JMP
